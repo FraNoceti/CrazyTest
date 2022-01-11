8e44eeb30c7d6ea8ff37e140601f4f4821cd269c
FraNoceti <francescoluiginoceti@gmail.com>
2022-01-12 00:07:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./cs.png";
+import "./App.css";
+import biglogo from "./biglogo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDiscord } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={biglogo} className="App-biglogo" alt="logo" />
+        </header>
+        <div className="Socialinks">
+          <p>Join the Club</p>
+        </div>
+        <div className="socials">
+            <FontAwesomeIcon
+              icon={faDiscord}
+              className="discord"></FontAwesomeIcon>
+              <FontAwesomeIcon
+              icon={faInstagram}
+              className="instagram"></FontAwesomeIcon>
+            <FontAwesomeIcon
+              icon={faTwitter}
+              className="twitter"
+            ></FontAwesomeIcon>
+            
+            <FontAwesomeIcon icon={faGithub} className="github"></FontAwesomeIcon>
+          </div>
+        
+      </div>
+    </>
   );
 }
 
