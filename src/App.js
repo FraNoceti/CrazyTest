a161f22703bfa25a8ee450c106800121faee20e9
FraNoceti <francescoluiginoceti@gmail.com>
2022-01-19 20:12:11 +0100

    ancora noi

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import logo from "./cs.png";
+import logo from "./CSy.png";
 import "./App.css";
-import biglogo from "./biglogo.png";
+import biglogo from "./bigglogo.png";
+import platforms from "./allplatforms.png"
+import join from "./Join3.png"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDiscord } from "@fortawesome/free-brands-svg-icons";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
@@ -15,24 +17,25 @@ function App() {
           <img src={logo} className="App-logo" alt="logo" />
           <img src={biglogo} className="App-biglogo" alt="logo" />
         </header>
-        <div className="Socialinks">
-          <p>Join the Club</p>
+        <img src={platforms} className="platforms" alt="logo" />
+        <div className="joindiv">
+          <img src={join} className="join" alt="logo" />
         </div>
         <div className="socials">
-            <FontAwesomeIcon
-              icon={faDiscord}
-              className="discord"></FontAwesomeIcon>
-              <FontAwesomeIcon
-              icon={faInstagram}
-              className="instagram"></FontAwesomeIcon>
-            <FontAwesomeIcon
-              icon={faTwitter}
-              className="twitter"
-            ></FontAwesomeIcon>
-            
-            <FontAwesomeIcon icon={faGithub} className="github"></FontAwesomeIcon>
-          </div>
-        
+          <FontAwesomeIcon
+            icon={faDiscord}
+            className="discord"
+          ></FontAwesomeIcon>
+          <FontAwesomeIcon
+            icon={faInstagram}
+            className="instagram"
+          ></FontAwesomeIcon>
+          <FontAwesomeIcon
+            icon={faTwitter}
+            className="twitter"
+          ></FontAwesomeIcon>
+          <FontAwesomeIcon icon={faGithub} className="github"></FontAwesomeIcon>
+        </div>
       </div>
     </>
   );
